aca5f6df42fdbb75fbd2eca9c3e690da6cf698c7
DK <dk6216720@gmail.com>
2023-07-10 20:10:58 -0400

    Binary tree base working

---
+++ script.js
@@ -0,0 +1,50 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(array) {
+    this.sortedArray = sortArray(array);
+    this.root = buildTree(this.sortedArray, 0, this.sortedArray.length - 1);
+  }
+}
+
+const sortArray = function (numbs) {
+  return numbs
+    .filter((numb, index) => numbs.indexOf(numb) === index)
+    .sort((a, b) => a - b);
+};
+
+const buildTree = function (array, start, end) {
+  if (start > end) return null;
+
+  const mid = Math.floor((start + end) / 2);
+  const root = new Node(array[mid]);
+
+  root.left = buildTree(array, start, mid - 1);
+  root.right = buildTree(array, mid + 1, end);
+
+  return root;
+};
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const data = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+const tree = new Tree(data);
+prettyPrint(tree.root);
